322e9205986cc3d62bea3af4d6da4cf88ac4b15f
jhonZevallos <zevallosjhon50@gmail.com>
2022-05-04 08:44:17 -0500

    RestTemplate - feing client

---
+++ usuario-service/src/main/java/com/usuario/service/servicio/UsuarioService.java
@@ -0,0 +1,99 @@
+package com.usuario.service.servicio;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.usuario.service.entidades.Usuario;
+import com.usuario.service.feignclient.CarroFeingClient;
+import com.usuario.service.feignclient.MotoFeignClient;
+import com.usuario.service.modelos.Carro;
+import com.usuario.service.modelos.Moto;
+import com.usuario.service.repositorio.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private CarroFeingClient carroFeingClient;
+
+	@Autowired
+	private MotoFeignClient motoFeignClient;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	// +++++++++++++++++++Rest template++++++++++++++++++++++++++++
+	public List<Carro> getCarros(int usuarioId) {
+		List<Carro> carros = restTemplate.getForObject("http://localhost:8002/carro/usuario/" + usuarioId, List.class);
+		return carros;
+	}
+
+	public List<Moto> getMotos(int usuarioId) {
+		List<Moto> motos = restTemplate.getForObject("http://localhost:8003/moto/usuario/" + usuarioId, List.class);
+		return motos;
+	}
+
+	// *********************FEIGN******************************
+	public Carro saveCarro(int usuarioId, Carro carro) {
+		carro.setUsuarioId(usuarioId);
+		Carro nuevocarro = carroFeingClient.save(carro);
+		return nuevocarro;
+	}
+
+	public Moto saveMoto(int usuarioId, Moto moto) {
+		moto.setUsuarioId(usuarioId);
+		Moto nuevaMoto = motoFeignClient.save(moto);
+		return nuevaMoto;
+	}
+
+	public Map<String, Object> getUsuarioAndVehiculos(int usuarioId) {
+		Map<String, Object> resultado = new HashMap<>();
+
+		Usuario usuario = usuarioRepository.findById(usuarioId).orElse(null);
+
+		if (usuario == null) {
+			resultado.put("Mensaje", "El usuario no existe");
+			return resultado;
+		}
+
+		resultado.put("Usuario", usuario);
+		List<Carro> carros = carroFeingClient.getCarros(usuarioId);
+		if (carros.isEmpty()) {
+			resultado.put("Carros", "El usuario no tiene carros");
+		} else {
+			resultado.put("Carros", carros);
+		}
+
+		List<Moto> motos = motoFeignClient.getMotos(usuarioId);
+		if (motos.isEmpty()) {
+			resultado.put("Motos", "El usuario no tiene motos");
+		} else {
+			resultado.put("Motos", motos);
+		}
+
+		return resultado;
+	}
+
+	// ---------------------------------------------------------------------------------------------------------------
+	public List<Usuario> getAll() {
+		return usuarioRepository.findAll();
+	}
+
+	public Usuario getUsuarioById(int id) {
+		return usuarioRepository.findById(id).orElse(null);
+	}
+
+	public Usuario save(Usuario usuario) {
+		Usuario nuevoUser = usuarioRepository.save(usuario);
+		return nuevoUser;
+	}
+
+}
